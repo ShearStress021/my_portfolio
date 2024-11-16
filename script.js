bf88dec01eacae1c33f594476f9d728d813f2b44
Samkanja <samuelkanja4@gmail.com>
2024-11-15 19:34:18 -0800

    header

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuList = document.querySelector('.menu-list')
+
+menuList.addEventListener('click', () => {
+    const menuItems = document.querySelector('.menu-items')
+    menuItems.style.display = 'block' 
+})
